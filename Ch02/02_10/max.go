36ee5071c1911e6134cdcec64b1206bdc2769c46
Myles Moylan <moylanm@fastmail.com>
2024-10-15 18:14:14 -0700

    Initialize max variable with first element of nums slice

--- Ch02/02_10/max.go
+++ Ch02/02_10/max.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	nums := []int{16, 8, 42, 4, 23, 15}
-	max := 0
+	max := nums[0]
 
-	for _, n := range nums {
+	for _, n := range nums[1:] {
 		if n > max {
 			max = n
 		}
