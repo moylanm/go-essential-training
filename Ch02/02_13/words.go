41608cda2ddb2ffd8068cf995ae22e46861bb398
Myles Moylan <moylanm@fastmail.com>
2024-10-16 07:36:29 -0700

    Simple maps challenge

--- Ch02/02_13/words.go
+++ Ch02/02_13/words.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -12,5 +13,14 @@ func main() {
 	To catch me the wind
 	`
 
-	fmt.Println(text)
+	words := strings.Fields(text)
+	wordMap := make(map[string]int)
+
+	for _, word := range words {
+		wordMap[word]++
+	}
+
+	for word, count := range wordMap {
+		fmt.Printf("%s: %d\n", word, count)
+	}
 }
