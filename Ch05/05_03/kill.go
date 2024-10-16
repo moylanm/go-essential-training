2fe9ebc5d71ae3674e2feaca3fe2bc8c0c74b856
Myles Moylan <moylanm@fastmail.com>
2024-10-16 10:53:39 -0700

    Error handling challenge

--- Ch05/05_03/kill.go
+++ Ch05/05_03/kill.go
@@ -3,13 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/pkg/errors"
 )
 
 func killServer(pidFile string) error {
-	// TODO
+	file, err := os.Open(pidFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var pid int
+	if _, err := fmt.Fscanf(file, "%d", &pid); err != nil {
+		return errors.Wrap(err, "bad process ID")
+	}
 
 	// Simulate kill
 	fmt.Printf("killing server with pid=%d\n", pid)
+
+	if err := os.Remove(pidFile); err != nil {
+		log.Printf("warning: cannot remove pid file - %s", err)
+	}
+
 	return nil
 }
 
