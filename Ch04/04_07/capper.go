98dceff252e6a00596b1ea46ab0a39c5ffb1714c
Myles Moylan <moylanm@fastmail.com>
2024-10-16 10:36:42 -0700

    Implement interface challenge

--- Ch04/04_07/capper.go
+++ Ch04/04_07/capper.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 // Capper implements io.Writer and turns everything to uppercase
 type Capper struct {
-	// TODO
+	wtr io.Writer
+}
+
+func (c *Capper) Write(data []byte) (int, error) {
+	diff := byte('a' - 'A')
+	out := make([]byte, len(data))
+
+	for i, c := range data {
+		if c >= 'a' && c <= 'z' {
+			c -= diff
+		}
+		out[i] = c
+	}
+
+	return c.wtr.Write(out)
 }
 
 func main() {
-	c := &Capper{os.Stdout}
+	c := &Capper{wtr: os.Stdout}
 	fmt.Fprintln(c, "Hello there")
 }
