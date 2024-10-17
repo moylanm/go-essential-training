4e5063feb35e67d52fa2ee7a0d0023bbdfff9c1d
Myles Moylan <moylanm@fastmail.com>
2024-10-16 17:42:22 -0700

    Test challenge attempt

--- Ch07/07_04/sqrt_test.go
+++ Ch07/07_04/sqrt_test.go
@@ -1,9 +1,89 @@
 package sqrt
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 )
 
+type testCase struct {
+	value	 float64
+	expected float64
+}
+
+func newTestCase(value, expected float64) testCase {
+	return testCase{
+		value: value,
+		expected: expected,
+	}
+}
+
+func parseTestCaseFile() ([]testCase, error) {
+	file, err := os.Open("sqrt_cases.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var testCases []testCase
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		stringValues := strings.Split(scanner.Text(), ",")
+		value, expected, err := parseFloats(stringValues)
+		if err != nil {
+			return []testCase{}, err 
+		}
+
+		testCase := newTestCase(value, expected)
+		testCases = append(testCases, testCase)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return []testCase{}, err
+	}
+
+	return testCases, nil
+}
+
+func parseFloats(values []string) (float64, float64, error) {
+	value, err := strconv.ParseFloat(values[0], 64)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+
+	expected, err := strconv.ParseFloat(values[1], 64)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+
+	return value, expected, nil
+}
+
+func almostEqual(v1, v2 float64) bool {
+	return Abs(v1 - v2) <= 0.001
+}
+
 func TestMany(t *testing.T) {
-	// Read test cases from "sqrt_case.csv" and check them
+	testCases, err := parseTestCaseFile()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%f", tc.value), func(t *testing.T) {
+			out, err := Sqrt(tc.value)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !almostEqual(out, tc.expected) {
+				t.Fatalf("%f != %f", out, tc.expected)
+			}
+		})
+	}
 }
