68fc1f68f2a1573e4765353b83226c03a348b3c4
Myles Moylan <moylanm@fastmail.com>
2024-10-17 04:35:29 -0700

    HTTP request challenge

--- Ch08/08_04/github_api.go
+++ Ch08/08_04/github_api.go
@@ -2,25 +2,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
 type User struct {
-	Login    string
-	Name     string
-	NumRepos string // from public_repos in JSON
+	Login    string `json:"login"`
+	Name     string `json:"name"`
+	NumRepos int	`json:"public_repos"`
 }
 
 // userInfo return information on github user
 func userInfo(login string) (*User, error) {
-	/* TODO:
-	Call the github API for a given login
-	e.g. https://api.github.com/users/tebeka
+	url := fmt.Sprintf("https://api.github.com/users/%s", login)
+	resp, err := http.Get(url)
+	if err != nil {
+		return &User{}, err
+	}
+	defer resp.Body.Close()
 
-	And return User struct
-	*/
-	return nil, nil
+	user := User{}
+
+	parser := json.NewDecoder(resp.Body)
+	if err = parser.Decode(&user); err != nil {
+		return &User{}, err
+	}
+
+	return &user, nil
 }
 
 func main() {
